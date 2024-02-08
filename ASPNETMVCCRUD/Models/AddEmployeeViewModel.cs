2ba92b52c578d9f237309f43221c97ac3dff7855
yohanawi <yohan.indrawansha2000@gmail.com>
2024-02-08 21:19:24 +0530

    Add project files.

---
+++ ASPNETMVCCRUD/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPNETMVCCRUD.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ ASPNETMVCCRUD/Models/Domain/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNETMVCCRUD.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department {  get; set; }
+    }
+}
